a459c197d6bfc26fc6f02c903095c93b52e84b4e
ayan-gorain <ayan.gorain2021@vitbhopal.ac.in>
2023-06-24 11:15:51 +0530

    yag

--- lib/pages/chat.dart
+++ lib/pages/chat.dart
@@ -1,9 +1,65 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
-class profilepage extends StatelessWidget {
+class profilepage extends StatefulWidget {
   const profilepage({super.key});
 
+  @override
+  State<profilepage> createState() => _profilepageState();
+}
+
+class _profilepageState extends State<profilepage> {
+  late File imageFile;
+  TextEditingController fullNameController=TextEditingController();
+
+
+
+  void selectImage(ImageSource source) async{
+   XFile? pickedFile= await ImagePicker().pickImage(source: source);
+   if(pickedFile != null){
+     CropImage(pickedFile);
+   }
+
+  }
+  void CropImage(XFile file) async{
+    File? croppedImage =await ImageCropper.cropImage(sourcePath:file,Path)
+
+  }
+
+
+
+  void showPhotoOptions(){
+    showDialog(context: context, builder: (context){
+      return AlertDialog(
+        title: Text(
+          "Upload Profile Photo"
+        ),
+        content: Column(
+          children: [
+            ListTile(
+              onTap: ()
+                {
+                  selectImage(ImageSource.gallery);
+                },
+              leading: Icon(Icons.photo_album),
+              title:Text("Select From Gallary")
+            ),
+            ListTile(
+                onTap: ()
+                {
+                  selectImage(ImageSource.camera);
+                },
+                leading: Icon(Icons.camera_alt),
+                title:Text("Select From Camera")
+            ),
+          ],
+        ),
+      );
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,7 +87,9 @@ class profilepage extends StatelessWidget {
             SizedBox(height: 150,),
             Center(
               child: CupertinoButton(
-                onPressed: (){},
+                onPressed: (){
+                  showPhotoOptions();
+                },
                 child: CircleAvatar(
                   radius: 69,
                   child: Icon(Icons.person,size:60),
